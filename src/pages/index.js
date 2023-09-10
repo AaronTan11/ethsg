e91020d5a55483f76b197382c0fac37e116d99da
ChuaWeiHan0924 <81673644+ChuaWeiHan0924@users.noreply.github.com>
2023-09-10 15:41:02 +0800

    Revert "added the pages"
    This reverts commit 7e6b73ef9b762e3143f66940a7c50130948eaa2b.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
-import Nav from 'components/navBar'
+import Image from "next/image";
 
 export default function Home() {
     return (
         <>
-        <Nav />
             <div className="flex justify-center items-center flex-col h-screen space-y-7">
                 <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
                     Portfolio Management
