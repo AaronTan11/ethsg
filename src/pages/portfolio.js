7e6b73ef9b762e3143f66940a7c50130948eaa2b
ChuaWeiHan0924 <81673644+ChuaWeiHan0924@users.noreply.github.com>
2023-09-10 15:39:02 +0800

    added the pages

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,12 +1,13 @@
+import React from 'react'
 import Nav from 'components/navBar'
 
-export default function Home() {
+export default function portfolio() {
     return (
         <>
         <Nav />
             <div className="flex justify-center items-center flex-col h-screen space-y-7">
                 <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                    Portfolio Management
+                    Portfolio
                 </h1>
             </div>
         </>
